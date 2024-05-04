30897f8246719dfa54f476eeb755683ca9ca7c51
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-05-04 13:09:54 +0530

    update

--- config/db.js
+++ config/db.js
@@ -4,8 +4,13 @@ const dotenv = require("dotenv").config();
 //connext to mongoode database useing Mongoose
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected....${conn.connection.host}`);
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            writeConcern: { w: "majority" },
+        })
+
     }
     catch (error) {
         console.log(`Error in connection ${error}`);
--- controller/Auth.js
+++ controller/Auth.js
@@ -84,7 +84,7 @@ exports.login = async (req, res) => {
             id: user._id,
             role: user.role,
         }
-        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        const token = jwt.sign(payLoad, process.env.JWT_SECRET, {
             expiresIn: "2h",
         });
 
